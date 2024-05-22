6ea87ab5495285e72ad696776dfa6f0aee52b6d1
Elvis Moses Tangasaiti <elvis.moses@student.moringaschool.com>
2024-05-22 16:47:36 +0300

    creates looping.py

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+      # code goes here!
+      counter = 10
+      while (counter > 0):
+          print(counter)
+          counter-=1
+          print("Happy New Year!")
 
+  
 def square_integers(int_list):
     # code goes here!
+    return [num ** 2 for num in int_list]
     pass
 
-def fizzbuzz():
+def fizzbuzz(num=None):
     # code goes here!
-    pass
+    if num is None:
+        for num in range(1, 101):
+            print(fizzbuzz(num))
+    else:
+        if num % 3 == 0 and num % 5 == 0:
+            return "FizzBuzz"
+        elif num % 3 == 0:
+            return "Fizz"
+        elif num % 5 == 0:
+            return "Buzz"
+        else:
+            return str(num)
